c2170997d1c3b942b5bca56a3125220470bba5bc
Camilo Rodríguez <aeroxmotion@hotmail.com>
2020-11-24 21:06:03 -0500

    chore(core/transformers): avoid using global in Map/Set constructors

--- packages/core/src/transformer.ts
+++ packages/core/src/transformer.ts
@@ -1,44 +1,33 @@
-const gl = typeof window !== 'undefined'
-  ? window
-  : global
+export type TransformFn<Result = any> = (value: any) => Result
 
-export type TransformFn = (value: any) => any
+const MapTransform: TransformFn<Map<any, any>> = value => {
+  if (Array.isArray(value)) {
+    return new Map(value)
+  }
 
-const Map: TransformFn = value => {
   if (typeof value !== 'object' || value === null) {
     throw new TypeError('Invalid value')
   }
 
-  const map = new gl.Map<any, any>()
+  const map = new Map()
+  const keys = Object.keys(value)
 
-  if (!Array.isArray(value)) {
-    const keys = Object.keys(value)
-
-    for (const key of keys) {
-      map.set(key, value[key])
-    }
-  } else {
-    for (const element of value) {
-      if (!Array.isArray(element) || element.length !== 2) {
-        throw new TypeError('Expecting an entry array: [key, value]')
-      }
-
-      map.set(...element as [any, any])
-    }
+  for (const key of keys) {
+    map.set(key, value[key])
   }
 
   return map
 }
 
-const Set: TransformFn = value => {
+const SetTransform: TransformFn<Set<any>> = value => {
   if (!Array.isArray(value)) {
     throw new TypeError('Invalid value')
   }
 
-  return new gl.Set<any>(value)
+  return new Set(value)
 }
 
 export const transformers = {
-  Map,
-  Set
+  Map: MapTransform,
+  Set: SetTransform
 }
